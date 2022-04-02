597ec0512abf695f1bf9751b1cdeccb7bd5421d4
goldipl <mgodlewski92@gmail.com>
2022-04-02 10:06:06 +0200

    add mouse move component

--- src/ex3/MouseMove.js
+++ src/ex3/MouseMove.js
@@ -1,11 +1,28 @@
 import { useEffect, useState } from "react";
 
 export const MouseMove = () => {
-useEffect(() => {
-    window.addEventListener('mousemove', (e) => {
-        console.log(e);
+    const [mousePosition, setMousePosition] = useState({
+        x: 0,
+        y: 0, 
     });
-}, []);
 
-    return <div>Mouse</div>
+    useEffect(() => {
+        window.addEventListener('mousemove', (e) => {
+            console.log(e.offsetX, e.offsetY);
+
+            setMousePosition({
+                x: e.offsetX,
+                y: e.offsetY,
+            })
+        });
+    }, []);
+
+    return (
+        <>
+            <div>Mouse</div>
+            <div>Mouse Position: </div>
+            <div>X: {mousePosition.x}</div>
+            <div>Y: {mousePosition.y}</div>
+        </>
+    );
 };
