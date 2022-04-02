86d03a78d7eaf37ff6f8f108efd5898a9f00d11c
goldipl <mgodlewski92@gmail.com>
2022-04-02 15:02:24 +0200

    add colorful buttons

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import { InputComp } from './ex4/InputComp';
 import { Container } from './ex5/Container';
 import { MouseMoveClass } from './ex3/MouseMoveClass';
 import { StylesTest } from './ex6/StylesTest';
+import { Buttons } from './ex7/Buttons';
 
 
 function App() {
   return (
     <div className="App">
-      <StylesTest />
+      <Buttons />
     </div>
   );
 }
---
+++ src/ex7/Buttons.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import styles from './styles.module.css';
+import classNames from 'classnames';
+
+export const Buttons = () => {
+
+    const [redBtnActive, setRedBtnActive] = useState(false);
+    const [greenBtnActive, setGreenBtnActive] = useState(false);
+    const [yellowBtnActive, setYellowBtnActive] = useState(false);
+    const [brownBtnActive, setBrownBtnActive] = useState(false);
+
+    return (
+    <div>
+        <div className={classNames(styles.container, {
+            [styles.greenButton]: greenBtnActive,
+            [styles.redButton]: redBtnActive,
+            [styles.yellowButton]: yellowBtnActive,
+            [styles.brownButton]: brownBtnActive,
+        })} />
+
+        <button onClick={() => {
+            setRedBtnActive(!redBtnActive);
+            setGreenBtnActive(false);
+            setYellowBtnActive(false);
+            setBrownBtnActive(false);
+        }}>Button Red</button>
+        <button onClick={() => {
+            setGreenBtnActive(!greenBtnActive);
+            setYellowBtnActive(false);
+            setRedBtnActive(false);
+            setBrownBtnActive(false);
+        }}>Button Green</button>
+        <button onClick={() => {
+            setYellowBtnActive(!yellowBtnActive);
+            setGreenBtnActive(false);
+            setBrownBtnActive(false);
+            setRedBtnActive(false);
+        }}>Button Yellow</button>
+        <button onClick={() => {
+            setBrownBtnActive(!brownBtnActive);
+            setGreenBtnActive(false);
+            setRedBtnActive(false);
+            setYellowBtnActive(false);
+        }}>Button Brown</button>
+    </div>
+    );
+};
