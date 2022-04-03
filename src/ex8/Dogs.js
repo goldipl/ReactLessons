ca4897b632266b173a2fa70776f7f2e86d259daf
goldipl <mgodlewski92@gmail.com>
2022-04-03 10:52:05 +0200

    add ```Dogs``` component using API

--- src/ex8/Dogs.js
+++ src/ex8/Dogs.js
@@ -1,3 +1,22 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+const DOG_API = 'https://dog.ceo/api/breeds/image/random';
+
 export const Dogs = () => {
-    return <div>Hau Hau</div>
+
+    const [dogImage, setDogImage] = useState('');
+
+    const fetchDog = async () => {
+        const res = await axios.get(DOG_API);
+        setDogImage(res.data.message);
+    }
+
+    return <div>
+        <div>Dogs</div>
+        <button onClick={fetchDog}>Fetch dog</button>
+        <div>
+            <img width="400" src={dogImage} />
+        </div>
+    </div>
 }
