261addf72425151b386723f295d30d3e6c24b03c
goldipl <mgodlewski92@gmail.com>
2022-04-03 09:18:02 +0200

    add styles to React Router nav

--- src/App.js
+++ src/App.js
@@ -7,12 +7,20 @@ import { Container } from './ex5/Container';
 import { MouseMoveClass } from './ex3/MouseMoveClass';
 import { StylesTest } from './ex6/StylesTest';
 import { Buttons } from './ex7/Buttons';
+import { Routes, Route, Link } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <Buttons />
+      <nav>
+        <Link className="link01" to="/">Main</Link>
+        <Link className="link01" to="/mouse-move">Mouse Move</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<StylesTest />} />
+        <Route path="/mouse-move" element={<MouseMove />} />
+      </Routes>
     </div>
   );
 }
